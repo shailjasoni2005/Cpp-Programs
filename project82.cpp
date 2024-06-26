56c1a2ffa3e5bdea926a59f09b2b5b8903453c97
shailjasoni2005 <144000431+shailjasoni2005@users.noreply.github.com>
2024-06-26 16:43:45 +0000

    cpp program

--- project82.cpp
+++ project82.cpp
@@ -1,6 +1,45 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout<<"etc";
-    return 0;
+
+class Student{
+    private:
+    int rollNo;
+    string name;
+    int maths;
+    int science;
+    int physics;
+    public:
+
+    Student(int r,string n,int m,int s,int p){
+        rollNo=r;
+        name=n;
+        maths=m;
+        science=s;
+        physics=p;
+
     }
+
+    int totalMarks(){
+        return maths+science+physics;
+    }
+
+    char Grade(){
+        float average=totalMarks()/3;
+        if(average<40){
+            return 'C';
+        }
+        else if(average>=40 && average<60){
+            return 'B';
+        }
+        else{
+            return 'A';
+        }
+    }
+};
+
+int main(){
+    Student s(173,"shailja",85,92,94);
+    cout<<"Total marks: "<<s.totalMarks()<<endl;
+    cout<<"Grade: "<<s.Grade()<<endl;
+    return 0;
+}
