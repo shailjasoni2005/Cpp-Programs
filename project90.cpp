cf40c6bda7d44734e1a4d6ea3dba12f4364ea9d4
shailjasoni2005 <144000431+shailjasoni2005@users.noreply.github.com>
2024-07-06 15:50:56 +0000

    inheritance

--- project90.cpp
+++ project90.cpp
@@ -42,3 +42,12 @@ int main(){
     // C.c=30;
     return 0;
 }
+
+
+/*Types of inheritance:-
+1.single  inheritance.
+2.multiple inheritance.
+3.multilevel inheritance.
+4.hierarchical  inheritance.
+5.multipath/hybrid inheritance.
+*/
--- project91.cpp
+++ project91.cpp
@@ -1,6 +1,46 @@
-/*Types of inheritance:-
-1.single  inheritance.
-2.multiple inheritance.
-3.multilevel inheritance.
-4.hierarchical  inheritance.
-5.multipath/hybrid inheritance.*/
+// Exercise:----
+
+#include<iostream>
+using namespace std;
+
+class employee{
+    private:
+    int empId;
+    string name;
+    public:
+    employee(int e,string n){
+        empId=e;
+        name=n;
+    }
+    int getEmpId(){return empId;}
+    string getName(){ return name;}
+};
+
+class FullTimeEMployee:public employee{
+    private:
+    int salary;
+    public: 
+    FullTimeEMployee(int e,string n,int s):employee(e,n){
+        salary=s;
+    }
+    int getSalary(){ return salary;}
+};
+
+class PartTimeEmployee:public employee{
+    private:
+    int wage;
+    public:
+    PartTimeEmployee(int e,string n,int w):employee(e,n){
+        wage=w;
+    }
+    int getWage(){return wage;}
+};
+
+int main(){
+    PartTimeEmployee p1(1,"jhon",300);
+    FullTimeEMployee p2(2,"lucifer",5000);
+
+    cout<<"salary of "<<p2.getName()<<"is "<<p2.getSalary()<<endl;
+    cout<<"wage of  "<<p1.getName()<<" is "<<p1.getWage()<<endl;
+    return 0;
+}
